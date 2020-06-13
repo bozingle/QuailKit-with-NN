193e8b0abb34833b322af76297bb5904d23b5fb9
FeriBolour <57817579+FeriBolour@users.noreply.github.com>
2020-06-13 17:22:06 -0500

    Parallel taken off

--- FBspectrogram.m
+++ FBspectrogram.m
@@ -1,49 +1,43 @@
 function s = FBspectrogram(app)
+s = cell(1,4);% t= cell(1,4);
+%column= [1,3,5,7];
+for i = 1:size(app.AudioFilePlay,2)
     
-    s = cell(1,4);% t= cell(1,4);
-    %column= [1,3,5,7];
-    Audio = app.AudioFilePlay;
-    Interval = app.subInterval(1):app.subInterval(2);
+    %x = (app.audioSamples(app.subInterval(1):app.subInterval(2),column(i)) + ...
+    %   app.audioSamples(app.subInterval(1):app.subInterval(2),column(i)+1))/2;
+    try
+        x = app.AudioFilePlay(app.subInterval(1):app.subInterval(2),i);
+    catch e
+        x = app.AudioFilePlay(app.subInterval(1):end,i);
+    end
     window = app.Fs*app.window_size;
-    F = app.F;
-    Fs = app.Fs;
     noverlap = round(app.noverlap_size*window);
-    parfor i = 1:size(Audio,2)
+    if window <= length(x)
+        s{i} = spectrogram(x,window,noverlap,app.F,app.Fs);
         
-        try
-            x = Audio(Interval,i);
-        catch e
-            x = Audio(Interval(1),i);
-        end
-           
-        if window <= length(x)
-            s{i} = spectrogram(x,window,noverlap,F,Fs);
-
-            s{i}= mat2gray(abs(s{i}));
-        else
-            s{i} = [];
-        end
-
+        s{i}= mat2gray(abs(s{i}));
+    else
+        s{i} = [];
     end
     
-    if strcmp(app.ModeSwitch.Value,"Online")
-        curtime = app.curLoadInterval*app.loadIntervalRate + app.curSubInterval*app.loadSubIntervalRate;
-        time = curtime:1:(curtime+10);
-        timestr = app.convertToRealTime(time);
-
-        imagesc(app.UIAxes,time,app.F,s{1})
-        imagesc(app.UIAxes_2,time,app.F,s{2})
-        imagesc(app.UIAxes_3,time,app.F,s{3})
-        imagesc(app.UIAxes_4,time,app.F,s{4})
-
-        app.UIAxes.YDir = 'normal'; app.UIAxes.XLim = [time(1),time(end)];
-        app.UIAxes_2.YDir = 'normal'; app.UIAxes_2.XLim = [time(1),time(end)];
-        app.UIAxes_3.YDir = 'normal'; app.UIAxes_3.XLim = [time(1),time(end)];
-        app.UIAxes_4.YDir = 'normal'; app.UIAxes_4.XLim = [time(1),time(end)];
-        app.UIAxes.YLim = [app.F(1),app.F(end)]; app.UIAxes.XTickLabel = timestr;
-        app.UIAxes_2.YLim = [app.F(1),app.F(end)]; app.UIAxes_2.XTickLabel = timestr;
-        app.UIAxes_3.YLim = [app.F(1),app.F(end)]; app.UIAxes_3.XTickLabel = timestr;
-        app.UIAxes_4.YLim = [app.F(1),app.F(end)]; app.UIAxes_4.XTickLabel = timestr;
-    end
+end
+if strcmp(app.ModeSwitch.Value,"Online")
+    curtime = app.curLoadInterval*app.loadIntervalRate + app.curSubInterval*app.loadSubIntervalRate;
+    time = curtime:1:(curtime+10);
+    timestr = app.convertToRealTime(time);
     
+    imagesc(app.UIAxes,time,app.F,s{1})
+    imagesc(app.UIAxes_2,time,app.F,s{2})
+    imagesc(app.UIAxes_3,time,app.F,s{3})
+    imagesc(app.UIAxes_4,time,app.F,s{4})
+    
+    app.UIAxes.YDir = 'normal'; app.UIAxes.XLim = [time(1),time(end)];
+    app.UIAxes_2.YDir = 'normal'; app.UIAxes_2.XLim = [time(1),time(end)];
+    app.UIAxes_3.YDir = 'normal'; app.UIAxes_3.XLim = [time(1),time(end)];
+    app.UIAxes_4.YDir = 'normal'; app.UIAxes_4.XLim = [time(1),time(end)];
+    app.UIAxes.YLim = [app.F(1),app.F(end)]; app.UIAxes.XTickLabel = timestr;
+    app.UIAxes_2.YLim = [app.F(1),app.F(end)]; app.UIAxes_2.XTickLabel = timestr;
+    app.UIAxes_3.YLim = [app.F(1),app.F(end)]; app.UIAxes_3.XTickLabel = timestr;
+    app.UIAxes_4.YLim = [app.F(1),app.F(end)]; app.UIAxes_4.XTickLabel = timestr;
+end
 end
--- QCallDetection.m
+++ QCallDetection.m
@@ -31,32 +31,36 @@ spec_duration = 10;
 %title('Template')
 CallA = [];CallB = [];CallC = [];CallD = [];
 %% Quail Call Detection
-Spectrograms = app.Spectrograms;
-parfor i = 1:4
-    Ispec= Spectrograms{i};
-    if ~isempty(Spectrograms{i})
+for i = 1:4
+    Ispec= app.Spectrograms{i};
+    if ~isempty(app.Spectrograms{i})
         spec_len=size(Ispec,2);
         spec_width=size(Ispec,1);
         %     figure();imshow(Ispec)
+        if strcmp(value,"Default Real Bird") || strcmp(value,"Import Real Bird")
+            band=round((spec_width-temp_width)/3);
+            spec_seg_x= band:size(Ispec,1)-band;
+        else
+            spec_seg_x= 1:size(Ispec,1);
+        end
         
-        band=round((spec_width-temp_width)/3);
         spec_seg_size=ceil(spec_len/ceil_x);
-        
+
         call_candidates=[];
         %call_locations=[];
         no_seg=ceil(spec_len/spec_seg_size)*2-1;
         for s=1:no_seg
-            spec_seg=Ispec(band:end-band,(s-1)*ceil(spec_seg_size/2)+1:min((s-1)*ceil(spec_seg_size/2)+1+spec_seg_size,spec_len));
-            
+            spec_seg=Ispec(spec_seg_x,(s-1)*ceil(spec_seg_size/2)+1:min((s-1)*ceil(spec_seg_size/2)+1+spec_seg_size,spec_len));
+
             cc=xcorr2(spec_seg,template);
             cc_signal=mat2gray(max(cc));
             cc_signal=cc_signal(round(temp_length/2)-1:end-round(temp_length/2));
-            
+
             TF = islocalmax(cc_signal,'MinProminence',MinProm_x,'ProminenceWindow',temp_length/2);
             locs=find(TF);
             call=(((s-1)*ceil(spec_seg_size/2)+1)+locs)';
             call_candidates=[call_candidates;call];
-            
+
             %         figure;imshow(spec_seg)
             %         x=1:length(cc_signal);
             %         figure;plot(x,cc_signal,x(TF),cc_signal(TF),'r*')
@@ -68,16 +72,16 @@ parfor i = 1:4
                 for k=1:n
                     [rows,~]=find(L==k);
                     rows=unique(rows);
-                    spec_seg=Ispec(band:end-band,max(call_candidates(min(rows))-...
+                    spec_seg=Ispec(spec_seg_x,max(call_candidates(min(rows))-...
                         round(temp_length/2),1):min(call_candidates(min(rows))+round(temp_length/2),spec_len));
-                    
+
                     cc=xcorr2(spec_seg,template);
                     cc_signal=mat2gray(max(cc));
                     cc_signal=cc_signal(round(temp_length/2)-1:end-round(temp_length/2));
-                    
+
                     TF = islocalmax(cc_signal,'MinProminence',MinProm_x,'ProminenceWindow',temp_length/2);
                     location=find(TF);
-                    
+
                     if call_candidates(min(rows)) <round(temp_length/2)
                         call=(time(1)+(spec_duration/spec_len)*location);
                         Calls{i}=[Calls{i};call'];
@@ -87,23 +91,23 @@ parfor i = 1:4
                         Calls{i}=[Calls{i};call'];
                         %call_locations=[call_locations;call_candidates(min(rows))+location'-round(temp_length/2)];
                     end
-                    
+
                     %                 figure;imshow(spec_seg)
                     %                 x=1:length(cc_signal);
                     %                 figure;plot(x,cc_signal,x(TF),cc_signal(TF),'r*')
                     %                 waitforbuttonpress;
                 end
             else
-                spec_seg=Ispec(band:end-band,max(call_candidates(1)-round(temp_length/2),1):min(call_candidates(1)+round(temp_length/2),spec_len));
-                
+                spec_seg=Ispec(spec_seg_x,max(call_candidates(1)-round(temp_length/2),1):min(call_candidates(1)+round(temp_length/2),spec_len));
+
                 cc=xcorr2(spec_seg,template);
                 cc_signal=mat2gray(max(cc));
                 cc_signal=cc_signal(round(temp_length/2)-1:end-round(temp_length/2));
-                
-                
+
+
                 TF = islocalmax(cc_signal,'MinProminence',MinProm_x,'ProminenceWindow',temp_length/2);
                 location=find(TF);
-                
+
                 if call_candidates(1) <round(temp_length/2)
                     call=(time(1)+(spec_duration/spec_len)*location)';
                     Calls{i}=[Calls{i};call'];
@@ -113,43 +117,59 @@ parfor i = 1:4
                     Calls{i}=[Calls{i};call'];
                     %call_locations=[call_locations;max(call_candidates(1)+location'-round(temp_length/2),1)];
                 end
-                
+
                 %             figure;imshow(spec_seg)
                 %             x=1:length(cc_signal);
                 %             figure;plot(x,cc_signal,x(TF),cc_signal(TF),'r*')
                 %             waitforbuttonpress;
             end
         end
-        
+
         % For Drawing Bounding Boxes
         %pos{i} = [Calls{i} 1200*ones(length(Calls{i}),1) ...
         %    0.5*ones(length(Calls{i}),1) 1900*ones(length(Calls{i}),1)];
-        
-    end
-end
-%% Place Call Times into corresponding variables
-CallA = Calls{1};
-CallA(find(CallA==0)) = [];
-CallB = Calls{2};
-CallB(find(CallB==0)) = [];
-CallC = Calls{3};
-CallC(find(CallC==0)) = [];
-CallD = Calls{4};
-CallD(find(CallD==0)) = [];
-%% Drawing Lines on Calls
-if app.OffButton.Value == 1 && strcmp(app.ModeSwitch.Value,"Online")
-    for row = 1:size(Calls{1},1)
-        line(app.UIAxes,Calls{1}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
-    end
-    for row = 1:size(Calls{2},1)
-        line(app.UIAxes_2,Calls{2}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
-    end
-    for row = 1:size(Calls{3},1)
-        line(app.UIAxes_3,Calls{3}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
-    end
-    for row = 1:size(Calls{4},1)
-        line(app.UIAxes_4,Calls{4}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
-    end
-end
 
+    
+    %% Place Call Times into corresponding variables
+    CallA = Calls{1};
+    CallA(find(CallA==0)) = [];
+    CallB = Calls{2};
+    CallB(find(CallB==0)) = [];
+    CallC = Calls{3};
+    CallC(find(CallC==0)) = [];
+    CallD = Calls{4};
+    CallD(find(CallD==0)) = [];
+     %% Drawing Lines on Calls
+     if app.OffButton.Value == 1 && strcmp(app.ModeSwitch.Value,"Online")
+         for row = 1:size(Calls{1},1)
+             line(app.UIAxes,Calls{1}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
+         end
+         for row = 1:size(Calls{2},1)
+             line(app.UIAxes_2,Calls{2}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
+         end
+         for row = 1:size(Calls{3},1)
+             line(app.UIAxes_3,Calls{3}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
+         end
+         for row = 1:size(Calls{4},1)
+             line(app.UIAxes_4,Calls{4}(row,:)*ones(1,length(app.F)),app.F,'Color','red','LineWidth',1.5);
+         end
+     end
+    end 
 end
+end
+ %% Drawing Bounding Boxes
+% for row = 1:size(pos{1},1)
+%     rectangle(app.UIAxes,'Position',pos{1}(row,:),'EdgeColor','red')
+% end
+% for row = 1:size(pos{2},1)
+%     rectangle(app.UIAxes_2,'Position',pos{2}(row,:),'EdgeColor','red')
+% end
+% for row = 1:size(pos{3},1)
+%     rectangle(app.UIAxes_3,'Position',pos{3}(row,:),'EdgeColor','red')
+% end
+% for row = 1:size(pos{4},1)
+%     rectangle(app.UIAxes_4,'Position',pos{4}(row,:),'EdgeColor','red')
+% end
+% 
+% end
+
